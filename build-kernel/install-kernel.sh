1e7a06e1edd296d503ea1deced64ba6b16fccd26
rlucente <rlucente@redhat.com>
2021-08-06 17:35:00 -0400

    do not enable binderfs

--- build-kernel/install-kernel.sh
+++ build-kernel/install-kernel.sh
@@ -3,8 +3,8 @@
 pushd $(dirname $0)/kernel/x86_64
 
 sudo dnf -y update
-#sudo rpm -ihv --force $(ls *.rpm | grep -v debug)
 sudo dnf -y install --nogpgcheck $(ls *.rpm | grep -v debug)
+sudo rpm -ihv --force $(ls *.rpm | grep -v debug)
 
 popd
 
